4d01aa0f281a6467ac4a081585a3d6ad27343f23
Babatunde Sanusi <swisskid95@gmail.com>
2019-10-05 01:13:33 +0100

    conclude the mvp

---
+++ public/precache-manifest.6d1ce97a7e87539320f9bd755c6c993e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734dc170d95fb5a81ca5cd0dba3a8947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b59b53e4551fd703eb",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "e147e4df6e1d365f824f",
+    "url": "/static/js/2.a292e8de.chunk.js"
+  },
+  {
+    "revision": "d9b59b53e4551fd703eb",
+    "url": "/static/js/main.9645c69b.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "14ef0d8d218e91b03489abbedd1d2681",
+    "url": "/static/media/logo.14ef0d8d.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema.js");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Allow cross origin requests
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -12,5 +17,11 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static("public"));
+
+app.get("*", (request, response) => {
+  response.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
